530f609bd455a79adf7e3d6b8cac34d00fc1cd87
sjamescarter <stephenjamescarter@gmail.com>
2023-03-17 12:13:12 -0500

    commit tree f627e93a18113f271264be6addf52d82f9560092

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    "#{array[0]}"
+  elsif array.length == 2
+    "#{array[0]} and #{array[1]}"
+  else
+    "#{array.slice(0...-1).join(", ")}, and #{array[-1]}"
+  end
 end
